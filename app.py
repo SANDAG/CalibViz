7d0d9885106706d8adbbb72c43fd988f97b84b25
Bhargava Sana <bhargavs_in@yahoo.com>
2025-05-09 17:47:16 -0700

    Inital commit for test dash app

---
+++ app.py
@@ -0,0 +1,13 @@
+import dash
+from dash import html
+
+app = dash.Dash(__name__)
+server = app.server  # This is required for Azure deployment
+
+app.layout = html.Div(children=[
+    html.H1("Hello Dash"),
+    html.P("This is a simple Dash app.")
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
